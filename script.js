36b5dedfeedd3e7838d1cd4bcc5ee558125db720
Zio Gregory Magugat <zimagugat@gmail.com>
2025-01-12 17:00:12 +0800

    added insert tab feature and initialization of tabs

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let links = ["https://www.youtube.com/embed/IDwj5XZCJrQ?si=ZWWJ2BpNmgkvQ_UE",
-"https://www.youtube.com/embed/4GpEbLOavVA?si=avC_OY-jBnKUjpsg",
-"https://www.youtube.com/embed/Sxxw3qtb3_g?si=rg-GMct9asV-hb_M"];
+tabLinks = [
+  { link: "https://www.youtube.com/embed/IDwj5XZCJrQ?si=ZWWJ2BpNmgkvQ_UE", title: "Title 1" },
+  { link: "https://www.youtube.com/embed/4GpEbLOavVA?si=avC_OY-jBnKUjpsg", title: "Title 2" },
+  { link: "https://www.youtube.com/embed/Sxxw3qtb3_g?si=rg-GMct9asV-hb_M", title: "Title 3" }
+];
 
 let switchTabs = (index) => {
     index = index - 1; // Adjust index to match the array's 0-based indexing
@@ -8,9 +10,9 @@ let switchTabs = (index) => {
     
     if (tabDisplays.length > 0) {
       const tabDisplay = tabDisplays[0]; // Access the first element with the class "tab-display"
-      if(index >= 0 && index < links.length) {
+      if(index >= 0 && index < tabLinks.length) {
         tabDisplay.innerHTML = `<iframe class="tab-frame" 
-                                  src="${links[index]}" 
+                                  src="${tabLinks[index].link}" 
                                   sandbox="allow-scripts allow-same-origin" 
                                   frameborder="0" 
                                   allow="autoplay; encrypted-media" 
@@ -23,3 +25,28 @@ let switchTabs = (index) => {
       console.error("No element with the class 'tab-display' found.");
     }
 };
+
+let addNewTab = () => {
+    let l = prompt("Enter a link:");
+    let t = prompt("Enter the title of tab:");
+    if (l.length > 0 && t.length > 0) {
+      tabLinks.push({ link: l, title: t });
+      initializeTabs();
+    } else {
+      alert("Invalid Link or Title");
+    }
+}
+
+const initializeTabs = () => {
+  const tabList = document.getElementsByClassName("tab-list")[0];
+  let strlst = "";
+
+  for (let i = 0; i < tabLinks.length; i++) {
+    strlst += `<li class="tab-href" onclick="switchTabs(${i+1})">${tabLinks[i].title}</a></li>`;
+  }
+
+  strlst += `<button class="tab-btn" onclick="addNewTab()">Add Tab</button>`;
+
+  tabList.innerHTML = strlst;
+
+}
